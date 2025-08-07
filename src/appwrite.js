9c1f39c1958c8b70ce5821690996e9bb77267662
Ray <rei@reiyua.lol>
2025-08-07 09:26:14 +0000

    me idiot and put wrong type of collection ID

--- src/appwrite.js
+++ src/appwrite.js
@@ -9,5 +9,5 @@ client
 export const databases = new Databases(client)
 
 export const DATABASE_ID = '68946afa0003682b1612'
-export const COLLECTION_ID = 'global_clicks'     
+export const COLLECTION_ID = '68946b0f0038d0cbdb28'     
 export const DOCUMENT_ID = 'global_counter'      
